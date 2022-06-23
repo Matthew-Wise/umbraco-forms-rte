cb5e78fae4001dc7e55437e71c93d072bed684a4
Aaron <aaron@ph-creative.com>
2022-06-23 19:41:26 +0100

    Moved app plugins to razor class library

---
+++ src/Mw.Umbraco.Forms.Rte.Backoffice/Composers/MwUmbracoFormsRteComposer.cs
@@ -0,0 +1,13 @@
+﻿using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+
+namespace Mw.Umbraco.Forms.Rte.Backoffice.Composers
+{
+    public class MwUmbracoFormsRteComposer : IComposer
+    {
+        public void Compose(IUmbracoBuilder builder)
+        {
+            builder.ManifestFilters().Append<MwUmbracoFormsRteManifestFilter>();
+        }
+    }
+}
